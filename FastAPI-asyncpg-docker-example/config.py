326398bb8c608f9f3c4bc8f864b4f79d061a32e3
ixcme <mokkyibragimov@gmail.com>
2025-04-04 10:40:20 +0500

    Add files via upload

---
+++ FastAPI-asyncpg-docker-example/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+
+# DATABASE_URL = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = "postgresql://neondb_owner:npg_jYEaulK1Zh2x@ep-raspy-darkness-a13y3b8j-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+
+
